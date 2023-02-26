08955858e25ed787683e0640df90169c4d0e7287
Rain Zhu <rain1995zhu@gmail.com>
2023-02-26 15:32:13 -0800

    add hello.js update console.og and return

--- hello.js
+++ hello.js
@@ -12,3 +12,23 @@ const sayHello1  = function (name) {
 sayHello1("Caliban");
 sayHello1("Miranda");
 sayHello1("Ferdinand");
+
+
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+
+const returnSayHello1  = function (name) {
+  return "Hello, " + name;
+}
+const greeting1 = returnSayHello('John');
+console.log(greeting);
