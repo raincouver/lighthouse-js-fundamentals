e815d6908ec1da42da83f5f3ebe2f49d5f734cb3
Rain Zhu <rain1995zhu@gmail.com>
2023-02-26 15:33:59 -0800

    add even.js

---
+++ even.js
@@ -0,0 +1,14 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const isEven1  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven1(10);
+const elevenIsEven = isEven1(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
