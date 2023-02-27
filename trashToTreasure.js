1faebceb215255e0f7ab32e5b689468e5a62176b
Rain Zhu <rain1995zhu@gmail.com>
2023-02-26 19:10:39 -0800

    update trashToTreasure.js

--- trashToTreasure.js
+++ trashToTreasure.js
@@ -24,11 +24,11 @@
 
 smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 });
 
-{
-  waste: 4,
-  recycling: 3,
-  compost: 5
-}
+// {
+//   waste: 4,
+//   recycling: 3,
+//   compost: 5
+// }
 
 const smartGarbage = function (trash, bins) {
   if (trash == 'waste'){
