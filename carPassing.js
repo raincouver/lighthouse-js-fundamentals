0767b1bd72ae88ddbf6a157c24571937d6c153dc
Rain Zhu <rain1995zhu@gmail.com>
2023-02-26 19:39:30 -0800

    update carPassing.js

---
+++ carPassing.js
@@ -0,0 +1,80 @@
+// Challenge
+// Instruction
+// Complete the logic in the function carPassing.
+
+// The function, carPassing(cars, speed), takes in an array of car objects, and the speed of a car as it passes the sensor.
+
+// This function should create a new object with a property called speed, and another property called time and add it to the cars array. We can retrieve the current time, for setting the time property, by using the Date.now() function, which is built into JavaScript!
+
+// Our function should return an array that includes all of the elements in cars as well as our new element.
+
+// Note
+// We may need to use JavaScript's handy push() method to add our new object to the cars array. Take a look at this previous exercise for a refresher.
+
+// If we called the function with the following arguments.
+
+// const cars = [
+//   {
+//     time: 1568329654807,
+//     speed: 40,
+//   },
+//   {
+//     time: 1568329821632,
+//     speed: 42,
+//   },
+//   {
+//     time: 1568331115463,
+//     speed: 35
+//   }
+// ]
+
+// const speed = 38
+
+// carPassing(cars, speed)
+// The carPassing function might return the array:
+
+// [
+//   {
+//     time: 1568329654807,
+//     speed: 40,
+//   },
+//   {
+//     time: 1568329821632,
+//     speed: 42,
+//   },
+//   {
+//     time: 1568331115463,
+//     speed: 35
+//   },
+//   {
+//     time: 1568431216417,
+//     speed: 38
+//   }
+// ]
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38
+
+const carPassing = function (cars, speed){
+  let x = {
+    time : Date.now(),
+    speed : speed
+  }
+  cars.push(x);
+  return(cars);
+}
+
